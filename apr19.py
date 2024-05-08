29d650dcc87a73b775c2bc5cc67bda3c685e6f42
alakhpandya <alakhpandya@gmail.com>
2024-05-08 10:13:27 +0530

    may8

--- apr19.py
+++ apr19.py
@@ -15,7 +15,7 @@
 
 
 # Ask 4 numbers from user, call them a, b, c & d. Now find average of a & b as well as average of c & d using avg() function that takes two arguments. Finally, add both the averages and only print the final answer.
-
+"""
 a = float(input("a: "))     # 10
 b = float(input("b: "))     # 20
 c = float(input("c: "))     # 30
@@ -31,18 +31,48 @@ def avg(x, y):
 
 ans = avg(a, b) + avg(c, d)     # ans = None + None
 print("Sum of averages =", ans)
-
+"""
 """
 a = 2
 b = 3
 a! + b! = 2 + 6 = 8
-
 c = 4
 d = 5
 c! + d! = 24 + 120 = 144
-
-avg. of fact = (8 + 144)/2 = 151
-
+avg. of fact = (8 + 144)/2 = 76
 e = 6
-avg.of fact + 6! = 151 + 720 = 871
+avg.of fact + 6! = 76 + 720 = 796
 """
+
+# Write a function that determines whether the integer given in its argument is prime or not. Use this function in the main program that takes an integer from user and prints whether it is prime or composite.
+
+def checkPrime(x):
+    end_point = int(x**0.5)+1
+    for i in range(2, end_point):
+        if x % i == 0:
+            return False
+    return True
+"""
+n = int(input("number: "))
+if not checkPrime(n):
+    print("Composite")
+else:
+    print("Prime")
+"""
+
+# Write a code that takes two integers from user and prints only prime numbers between them.
+"""
+def checkPrime2(x):
+    end_point = int(x**0.5)+1
+    for i in range(2, end_point):
+        if x % i == 0:
+            print("Composite")
+            break
+    else:
+        print("Prime")
+"""
+a = int(input("a: "))
+b = int(input("b: "))
+for i in range(a, b+1):
+    if checkPrime(i):
+        print(i)
